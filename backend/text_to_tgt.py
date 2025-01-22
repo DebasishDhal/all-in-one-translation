3becfb17f3974dbe9b6d4a99053392d5a9c15eeb
Debasish Dhal <debasishdhal99ddhal@gmail.com>
2025-01-22 13:09:37 +0000

    error_correction

--- backend/text_to_tgt.py
+++ backend/text_to_tgt.py
@@ -1,5 +1,5 @@
 from deep_translator import GoogleTranslator
 
 def src_txt_to_eng_translator(input_text):
-    translated = GoogleTranslator(source='auto', target='en').translate(input)
+    translated = GoogleTranslator(source='auto', target='en').translate(input_text)
     return translated
