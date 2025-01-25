04cc3917b7f9a38dd0bccf04d814c6ba7c2840f6
Debasish Dhal <debasishdhal99ddhal@gmail.com>
2025-01-25 13:30:40 +0000

    Add basic image OCR in indian and western languages

--- backend/image_to_tgt.py
+++ backend/image_to_tgt.py
@@ -1,4 +1,22 @@
-from PIL import Image
+# from PIL import Image
 
-def src_image_to_eng_translator(image_input_file):
-    return "Random"
+# def src_image_to_eng_translator(image_input_file):
+#     return "Random"
+from deep_translator import GoogleTranslator
+import pytesseract
+from PIL import image
+
+language_dict = {'English': 'eng', 'French': 'fra', 'Odia': 'ori', 'Hindi': 'hin',
+                 'Bengali': 'ben', 'Telugu': 'tel', 'Hindi': 'hin', 'Malayalam': 'mal',
+                 'Kannada': 'kan', 'Tamil': 'tam', 'Marathi': 'mar', 'Gujarati': 'guj', 
+                 'Punjabi': 'pan', 'Sinhalese': 'sin',
+                 'Arabic': 'ara', 'German': 'deu', 'Spanish': 'spa', 'Italian': 'ita',
+                 'Russian': 'rus', 'Japanese': 'jpn', 'Korean': 'kor', 'Hebrew': 'heb',
+                 'Persian': 'fas', 'Chinese Simplified': 'chi_sim', 'Chinese Traditional': 'chi_tra',
+                 }
+
+def src_image_to_eng_translator(input_image, lang = 'eng'):
+    image_text = pytesseract.image_to_string(input_image, lang = language_dict.get(lang, 'eng'))
+    
+    translated = GoogleTranslator(source='auto', target='en').translate(text)
+    return image_text, translated
