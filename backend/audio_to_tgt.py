6b1b18f5f0e19078231d1ede704fa6ed228cd2f3
Debasish Dhal <debasishdhal99ddhal@gmail.com>
2025-01-26 14:02:27 +0000

    Add input audio processing

--- backend/audio_to_tgt.py
+++ backend/audio_to_tgt.py
@@ -6,8 +6,9 @@ client = OpenAI(api_key = whisper_key)
 
 
 def src_audio_to_eng_translator(audio_file_input):
-    transcription = client.audio.translations.create(
-    model="whisper-1", 
-    file=audio_file_input,
-    )
+    with open(audio_file_input, "rb") as audio_file:
+            transcription = client.audio.translations.create(
+                model="whisper-1", 
+                file=audio_file
+            )
     return transcription.text
