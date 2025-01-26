66dff0f49f2a605b0b68dcc0f2a4be4ba8572eb8
Debasish Dhal <debasishdhal99ddhal@gmail.com>
2025-01-26 14:09:51 +0000

    replace openai library with whisper

--- backend/audio_to_tgt.py
+++ backend/audio_to_tgt.py
@@ -1,14 +1,17 @@
 from openai import OpenAI
+import whisper
 import os
 
-whisper_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key = whisper_key)
+# whisper_key = os.getenv("OPENAI_API_KEY")
+# client = OpenAI(api_key = whisper_key)
 
 
 def src_audio_to_eng_translator(audio_file_input):
     with open(audio_file_input, "rb") as audio_file:
-            transcription = client.audio.translations.create(
-                model="whisper-1", 
-                file=audio_file
-            )
-    return transcription.text
+            # transcription = client.audio.translations.create(
+            #     model="whisper-1", 
+            #     file=audio_file
+            # )
+        model = whisper.load_model("turbo")
+        result = model.transcribe(audio_file)
+    return result.text
