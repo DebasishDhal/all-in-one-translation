1bce6389ce7ab91b16b868dc720055d8163e0ab2
Debasish Dhal <debasishdhal99ddhal@gmail.com>
2025-01-26 13:36:49 +0000

    add whisper to backend
    I should do the testing on colab, but testing is easy here because of microphone option i believe

--- backend/audio_to_tgt.py
+++ backend/audio_to_tgt.py
@@ -1,2 +1,6 @@
 def src_audio_to_eng_translator(audio_file_input):
-    return "Random"
+    transcription = client.audio.translations.create(
+    model="whisper-1", 
+    file=audio_file_input,
+    )
+    return transcription.text
